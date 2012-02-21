fb129d664da28b0946fe8f48916a629a42be8a31
Jason Robb <jlrobb@irco.com>
2012-02-21 11:56:32 -0700

    Load config yaml through ERB and refactoring

--- lib/brandy.rb
+++ lib/brandy.rb
@@ -3,17 +3,11 @@ require "brandy/brandy_helper"
 
 module Brandy
   class << self
-    attr_accessor :config
     #used to return a value from YAML file. Brandy.<string_to_return>({:request => request})
     #TODO: Load brand filtering options based on files in filters directory
-    def method_missing(name, args)
+    def method_missing(name, args = {})
       #name - key in yaml file to load (eg system, system_plain, domain)
       #args - hash of filters to use, evaluated in order. Options are independent of order (NYI)
-      
-      #hack method to load yaml if it hasn't been yet
-      if @config.nil?
-       @config = YAML.load_file(Rails.root.join("config","branding.yml"))
-      end
       
       if args.has_key?(:country_code)
         brand = brand_from_country_code args[:country_code]
@@ -30,11 +24,11 @@ module Brandy
     end
    
     def get_property(brand, property)
-     @config['brands'][brand.to_s][property.to_s]  
+     config['brands'][brand.to_s][property.to_s]
     end
     
     def brand_from_country_code country_code
-      @config['domains'][country_code.downcase] || @config['domains']['com']
+      config['domains'][country_code.downcase] || config['domains']['com'] || 'nexia'
     end
     
     def tld request
@@ -42,13 +36,15 @@ module Brandy
     end
     
     def brand_from_request request
-      @config['domains'][tld(request)] || @config['domains']['com']
-    end    
+      config['domains'][tld(request)] || config['domains']['com'] || 'nexia'
+    end
+    
     def prefix request
-      if @config.nil?
-       @config = YAML.load_file(Rails.root.join("config","branding.yml"))
-      end
-        @config['domains'][tld(request)] || @config['domains']['com'] || 'nexia'
+      config['domains'][tld(request)] || config['domains']['com'] || 'nexia'
+    end
+    
+    def config
+      @config ||= YAML.load(ERB.new(File.read(Rails.root.join('config', 'branding.yml'))).result)
     end
   end
 end
